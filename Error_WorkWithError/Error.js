84d24528ec666a710e031c09cca6ee460c85afb3
SokataGintoki <catarsislogos@gmail.com>
2023-07-21 13:58:39 +0300

    Ошибки. Как избежать поломки своего кода

---
+++ Error_WorkWithError/Error.js
@@ -0,0 +1,25 @@
+'use strict';
+
+try{
+    document.querySelector('.active').addEventListener('click', () => {
+        console.log('click-clack');
+    });
+} catch(e) {
+    console.log(e);
+}
+
+console.log('normal');
+
+// try {
+//     console.log('Normal');
+//     console.log(a);
+//     console.log('result');
+// } catch(error) {
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+// } finally {
+    
+// }
+
+// console.log('Still normal');
